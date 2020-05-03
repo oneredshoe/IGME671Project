2dbfbfb5828da4e1a5a864e051d33f687fa160a3
Wren B <lxb7132@rit.edu>
2020-05-03 19:01:33 -0400

    Mixed some more sounds, fixed up the volume and made sure the buttons event works because it broke at some point, added updated documentation and asset list

--- A Crude Brew/Assets/Scripts/ActiveOrderTracker.cs
+++ A Crude Brew/Assets/Scripts/ActiveOrderTracker.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f939ccd6945fc582ac952442c0e4169192fdfb17daeb5c49888184a65cbc32be
-size 7654
+oid sha256:ffaa462e50a181751ab7fa8779dd34a06533fe1a99b7299b845598db70d57d6e
+size 7722
